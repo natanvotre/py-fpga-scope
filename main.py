912c8c8eef60967d55feea22cfac182f72ac42be
Natan Votre <nvt@brickabode.com>
2021-01-31 20:00:11 -0300

    add FFT analysis

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
+from time import sleep
+from numpy import ndarray
 
 from src.quartus_api import InSystemController
+
+
+def calc_fft(data: ndarray, N=None, is_complex=False):
+    if N is None:
+        N = int(len(data)/2)*2
+
+    windowed_data = data * np.hanning(len(data))
+    result = 20*np.log10(
+        np.abs(
+            np.fft.fft(windowed_data, N)
+        ) / N
+    )
+    if is_complex:
+        data = np.zeros(N)
+        data[:int(N/2)] = result[int(N/2):]
+        data[int(N/2):] = result[:int(N/2)]
+        return data
+
+    return result[:int(N/2)]
+
+
+def show_fft(data: ndarray, fs=48e3, N=None, is_complex=False, show=True, name=None):
+    if N is None:
+        N = int(len(data)/2)*2
+    if is_complex:
+        f = np.linspace(-fs/2, fs/2, N)
+    else:
+        f = np.linspace(0, fs/2, int(N/2))
+    fft = calc_fft(data, N, is_complex)
+    plt.clf()
+    plt.plot(f, fft)
+    if show:
+        plt.show()
+    else:
+        plt.savefig(name)
+
 
 def main():
     base = InSystemController()
@@ -30,9 +67,8 @@ def main():
     print(hex(data[-1]))
     plt.plot(data)
     plt.show()
-    plt.plot(np.fft.fft(data, n=len(data)*10))
-    plt.show()
-    sleep(1)
+    show_fft(data, fs=1e6, N=10000)
+
 
 if __name__ == '__main__':
     main()
